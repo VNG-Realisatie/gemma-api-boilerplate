4fb9152a0a55a4b06cf1ef6f7ba34fddde736157
Steven Bal <steven@maykinmedia.nl>
2019-10-14 16:39:54 +0200

    Remove manual updating of default inspectors from api settings

--- src/project_name/conf/api.py
+++ src/project_name/conf/api.py
@@ -6,9 +6,6 @@ SECURITY_DEFINITION_NAME = 'JWT-Claims'
 
 SWAGGER_SETTINGS = BASE_SWAGGER_SETTINGS.copy()
 
-_default_field_inspectors = list(BASE_SWAGGER_SETTINGS['DEFAULT_FIELD_INSPECTORS'])
-_default_field_inspectors.remove('vng_api_common.inspectors.geojson.GeometryFieldInspector')
-
 SWAGGER_SETTINGS.update({
     'DEFAULT_INFO': '{{ project_name|lower }}.api.schema.info',
 
@@ -27,8 +24,6 @@ SWAGGER_SETTINGS.update({
             # 'type': 'apiKey',
         }
     },
-
-    'DEFAULT_FIELD_INSPECTORS': tuple(_default_field_inspectors),
 })
 
 GEMMA_URL_INFORMATIEMODEL_VERSIE = '1.0'
